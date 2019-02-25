32e4b23e92a5432676fa5d611bf54b713121d421
Ignacio de Osma <Ignacio@Ignacios-MBP.attlocal.net>
2019-02-25 01:32:46 -0500

    final results

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -4,134 +4,213 @@ from util import *
 
 class TowerOfHanoiGame(GameMaster):
 
-    def __init__(self):
-        super().__init__()
-        
-    def produceMovableQuery(self):
-        """
-        See overridden parent class method for more information.
+	def __init__(self):
+		super().__init__()
 
-        Returns:
-             A Fact object that could be used to query the currently available moves
-        """
-        return parse_input('fact: (movable ?disk ?init ?target)')
+	def produceMovableQuery(self):
+		"""
+		See overridden parent class method for more information.
 
-    def getGameState(self):
-        """
-        Returns a representation of the game in the current state.
-        The output should be a Tuple of three Tuples. Each inner tuple should
-        represent a peg, and its content the disks on the peg. Disks
-        should be represented by integers, with the smallest disk
-        represented by 1, and the second smallest 2, etc.
+		Returns:
+			 A Fact object that could be used to query the currently available moves
+		"""
+		return parse_input('fact: (movable ?disk ?init ?target)')
 
-        Within each inner Tuple, the integers should be sorted in ascending order,
-        indicating the smallest disk stacked on top of the larger ones.
+	def getGameState(self):
+		"""
+		Returns a representation of the game in the current state.
+		The output should be a Tuple of three Tuples. Each inner tuple should
+		represent a peg, and its content the disks on the peg. Disks
+		should be represented by integers, with the smallest disk
+		represented by 1, and the second smallest 2, etc.
 
-        For example, the output should adopt the following format:
-        ((1,2,5),(),(3, 4))
+		Within each inner Tuple, the integers should be sorted in ascending order,
+		indicating the smallest disk stacked on top of the larger ones.
 
-        Returns:
-            A Tuple of Tuples that represent the game state
-        """
-        ### student code goes here
-        pass
+		For example, the output should adopt the following format:
+		((1,2,5),(),(3, 4))
 
-    def makeMove(self, movable_statement):
-        """
-        Takes a MOVABLE statement and makes the corresponding move. This will
-        result in a change of the game state, and therefore requires updating
-        the KB in the Game Master.
+		Returns:
+			A Tuple of Tuples that represent the game state
+		"""
+		firstPeg = []
+		secondPeg = []
+		thirdPeg = []
+		inputParse = parse_input("fact: (inst ?peg peg)")
+		pegs = self.kb.kb_ask(inputParse)
+		for peg in pegs:
+			if not Fact(instantiate(Statement(("empty", "?peg")), peg)) in self.kb.facts:
+				# if not empty, get list of all disks on it
+				disks = self.kb.kb_ask(Fact(instantiate(Statement(("on", "?disk", "?peg")), peg)))
+				disksAsInts = []
+				for disk in disks:
+					entry = int(disk.bindings_dict["?disk"][4])
+					disksAsInts.append(entry)
+				pegAsInt = int(peg.bindings_dict["?peg"][3])  # get this peg's identity
+				if pegAsInt == 1:
+					if disksAsInts:
+						while disksAsInts:
+							smallestDisk = min(disksAsInts)
+							firstPeg.append(smallestDisk)
+							disksAsInts.remove(smallestDisk)
+				elif pegAsInt == 2:
+					if disksAsInts:
+						while disksAsInts:
+							smallestDisk = min(disksAsInts)
+							secondPeg.append(smallestDisk)
+							disksAsInts.remove(smallestDisk)
+				else:
+					if disksAsInts:
+						while disksAsInts:
+							smallestDisk = min(disksAsInts)
+							thirdPeg.append(smallestDisk)
+							disksAsInts.remove(smallestDisk)
+		result = (tuple(firstPeg), tuple(secondPeg), tuple(thirdPeg))
+		return result
 
-        The statement should come directly from the result of the MOVABLE query
-        issued to the KB, in the following format:
-        (movable disk1 peg1 peg3)
+	def makeMove(self, movable_statement):
+		"""
+		Takes a MOVABLE statement and makes the corresponding move. This will
+		result in a change of the game state, and therefore requires updating
+		the KB in the Game Master.
 
-        Args:
-            movable_statement: A Statement object that contains one of the currently viable moves
+		The statement should come directly from the result of the MOVABLE query
+		issued to the KB, in the following format:
+		(movable disk1 peg1 peg3)
 
-        Returns:
-            None
-        """
-        ### Student code goes here
-        pass
+		Args:
+			movable_statement: A Statement object that contains one of the currently viable moves
 
-    def reverseMove(self, movable_statement):
-        """
-        See overridden parent class method for more information.
+		Returns:
+			None
+		"""
+		statementTerms = movable_statement.terms
+		secondTerm = statementTerms[1]
+		lastTerm = statementTerms[2]
+		firstTerm = statementTerms[0]
 
-        Args:
-            movable_statement: A Statement object that contains one of the previously viable moves
+		newFirstDisk = self.kb.kb_ask(Fact(Statement(("onTop", firstTerm, "?obj"))))[0]
+		currFirstDisk = self.kb.kb_ask(Fact(Statement(("top", "?obj", lastTerm))))[0]
 
-        Returns:
-            None
-        """
-        pred = movable_statement.predicate
-        sl = movable_statement.terms
-        newList = [pred, sl[0], sl[2], sl[1]]
-        self.makeMove(Statement(newList))
+		newFirstFact = Fact(instantiate(Statement(("onTop", firstTerm, "?obj")), newFirstDisk))
+		currFirstFact = Fact(instantiate(Statement(("top", "?obj", lastTerm)), currFirstDisk))
+
+		newTopFact = Fact(instantiate(Statement(("top", "?obj", secondTerm)), newFirstDisk))
+		currTopFact = Fact(instantiate(Statement(("onTop", firstTerm, "?obj")), currFirstDisk))
+
+		self.kb.kb_retract(Fact(Statement(("on", firstTerm, secondTerm))))
+		self.kb.kb_retract(Fact(Statement(("top", firstTerm, secondTerm))))
+		self.kb.kb_retract(newFirstFact)
+		self.kb.kb_retract(currFirstFact)
+		self.kb.kb_assert(Fact(Statement(("on", firstTerm, lastTerm))))
+		self.kb.kb_assert(Fact(Statement(("top", firstTerm, lastTerm))))
+		self.kb.kb_assert(newTopFact)
+		self.kb.kb_assert(currTopFact)
+
+
+	def reverseMove(self, movable_statement):
+		"""
+		See overridden parent class method for more information.
+
+		Args:
+			movable_statement: A Statement object that contains one of the previously viable moves
+
+		Returns:
+			None
+		"""
+		pred = movable_statement.predicate
+		sl = movable_statement.terms
+		newList = [pred, sl[0], sl[2], sl[1]]
+		self.makeMove(Statement(newList))
 
 class Puzzle8Game(GameMaster):
 
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-    def produceMovableQuery(self):
-        """
-        Create the Fact object that could be used to query
-        the KB of the presently available moves. This function
-        is called once per game.
+	def produceMovableQuery(self):
+		"""
+		Create the Fact object that could be used to query
+		the KB of the presently available moves. This function
+		is called once per game.
 
-        Returns:
-             A Fact object that could be used to query the currently available moves
-        """
-        return parse_input('fact: (movable ?piece ?initX ?initY ?targetX ?targetY)')
+		Returns:
+			 A Fact object that could be used to query the currently available moves
+		"""
+		return parse_input('fact: (movable ?piece ?initX ?initY ?targetX ?targetY)')
 
-    def getGameState(self):
-        """
-        Returns a representation of the the game board in the current state.
-        The output should be a Tuple of Three Tuples. Each inner tuple should
-        represent a row of tiles on the board. Each tile should be represented
-        with an integer; the empty space should be represented with -1.
+	def getGameState(self):
+		"""
+		Returns a representation of the the game board in the current state.
+		The output should be a Tuple of Three Tuples. Each inner tuple should
+		represent a row of tiles on the board. Each tile should be represented
+		with an integer; the empty space should be represented with -1.
 
-        For example, the output should adopt the following format:
-        ((1, 2, 3), (4, 5, 6), (7, 8, -1))
+		For example, the output should adopt the following format:
+		((1, 2, 3), (4, 5, 6), (7, 8, -1))
 
-        Returns:
-            A Tuple of Tuples that represent the game state
-        """
-        ### Student code goes here
-        pass
+		Returns:
+			A Tuple of Tuples that represent the game state
+		"""
+		coordinates = {"pos1": ("pos1", "pos2", "pos3"),
+				"pos2": ("pos1", "pos2", "pos3"),
+				"pos3": ("pos1", "pos2", "pos3")}
+		currState = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+		for yCoord in coordinates:
+			y = int(yCoord[3]) - 1
+			for xCoord in coordinates[yCoord]:
+				x = int(xCoord[3]) - 1
+				question = Fact(Statement(["coord", "?tile", xCoord, yCoord]))
+				bindings = self.kb.kb_ask(question)[0]
+				tile = bindings.bindings_dict["?tile"][4]
+				tileAsInt = 0
+				if tile == 'y':
+					tileAsInt = -1
+				else:
+					tileAsInt = int(tile)
+				currState[y][x] = tileAsInt
+		return tuple([tuple(currState[0]), tuple(currState[1]), tuple(currState[2])])
 
-    def makeMove(self, movable_statement):
-        """
-        Takes a MOVABLE statement and makes the corresponding move. This will
-        result in a change of the game state, and therefore requires updating
-        the KB in the Game Master.
+	def makeMove(self, movable_statement):
+		"""
+		Takes a MOVABLE statement and makes the corresponding move. This will
+		result in a change of the game state, and therefore requires updating
+		the KB in the Game Master.
 
-        The statement should come directly from the result of the MOVABLE query
-        issued to the KB, in the following format:
-        (movable tile3 pos1 pos3 pos2 pos3)
+		The statement should come directly from the result of the MOVABLE query
+		issued to the KB, in the following format:
+		(movable tile3 pos1 pos3 pos2 pos3)
 
-        Args:
-            movable_statement: A Statement object that contains one of the currently viable moves
+		Args:
+			movable_statement: A Statement object that contains one of the currently viable moves
 
-        Returns:
-            None
-        """
-        ### Student code goes here
-        pass
+		Returns:
+			None
+		"""
+		terms = movable_statement.terms
+		currTile = terms[0]
+		currXCoord = terms[1]
+		newXCoord = terms[3]
+		currYCoord = terms[2]
+		newYCoord = terms[4]
 
-    def reverseMove(self, movable_statement):
-        """
-        See overridden parent class method for more information.
+		self.kb.kb_retract(Fact(Statement(("coordinate", "empty", newXCoord, newYCoord))))
+		self.kb.kb_retract(Fact(Statement(("coordinate", currTile, currXCoord, currYCoord))))
 
-        Args:
-            movable_statement: A Statement object that contains one of the previously viable moves
+		self.kb.kb_assert(Fact(Statement(("coordinate", "empty", currXCoord, currYCoord))))
+		self.kb.kb_assert(Fact(Statement(("coordinate", currTile, newXCoord, newYCoord))))
 
-        Returns:
-            None
-        """
-        pred = movable_statement.predicate
-        sl = movable_statement.terms
-        newList = [pred, sl[0], sl[3], sl[4], sl[1], sl[2]]
-        self.makeMove(Statement(newList))
+	def reverseMove(self, movable_statement):
+		"""
+		See overridden parent class method for more information.
+
+		Args:
+			movable_statement: A Statement object that contains one of the previously viable moves
+
+		Returns:
+			None
+		"""
+		pred = movable_statement.predicate
+		sl = movable_statement.terms
+		newList = [pred, sl[0], sl[3], sl[4], sl[1], sl[2]]
+		self.makeMove(Statement(newList))
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -2,42 +2,130 @@
 from solver import *
 
 class SolverDFS(UninformedSolver):
-    def __init__(self, gameMaster, victoryCondition):
-        super().__init__(gameMaster, victoryCondition)
+	def __init__(self, gameMaster, victoryCondition):
+		super().__init__(gameMaster, victoryCondition)
 
-    def solveOneStep(self):
-        """
-        Go to the next state that has not been explored. If a
-        game state leads to more than one unexplored game states,
-        explore in the order implied by the GameMaster.getMovables()
-        function.
-        If all game states reachable from a parent state has been explored,
-        the next explored state should conform to the specifications of
-        the Depth-First Search algorithm.
+	def solveOneStep(self):
+		"""
+		Go to the next state that has not been explored. If a
+		game state leads to more than one unexplored game states,
+		explore in the order implied by the GameMaster.getMovables()
+		function.
+		If all game states reachable from a parent state has been explored,
+		the next explored state should conform to the specifications of
+		the Depth-First Search algorithm.
 
-        Returns:
-            True if the desired solution state is reached, False otherwise
-        """
-        ### Student code goes here
-        return True
+		Returns:
+			True if the desired solution state is reached, False otherwise
+		"""
+		if self.currentState.state == self.victoryCondition:
+			return True
+		movesToMake = self.gm.getMovables()
+		depthOfChild = self.currentState.depth + 1
+		currMove = False
+		while not currMove:
+			curr = self.currentState.nextChildToVisit
+			if len(movesToMake) <= curr:
+				if self.currentState.parent:
+					self.gm.reverseMove(self.currentState.requiredMovable)
+					movesToMake = self.gm.getMovables()
+					self.currentState = self.currentState.parent
+					depthOfChild = self.currentState.depth + 1
+					continue
+				else:
+					return False
+			currMoveTry = movesToMake[curr]
+			self.gm.makeMove(currMoveTry)
+			resultingState = GameState(self.gm.getGameState(), depthOfChild, currMoveTry)
+			if resultingState not in self.visited:
+				resultingState.parent = self.currentState
+				self.currentState.children.append(resultingState)
+				self.currentState.nextChildToVisit += 1
+				self.currentState = resultingState
+				currMove = currMoveTry
+			else:
+				self.gm.reverseMove(currMoveTry)
+				self.currentState.nextChildToVisit += 1
+		if self.currentState.state == self.victoryCondition:
+			return True
+		else:
+			self.visited[self.currentState] = True
+			return False
+		return True
 
 
 class SolverBFS(UninformedSolver):
-    def __init__(self, gameMaster, victoryCondition):
-        super().__init__(gameMaster, victoryCondition)
+	def __init__(self, gameMaster, victoryCondition):
+		super().__init__(gameMaster, victoryCondition)
 
-    def solveOneStep(self):
-        """
-        Go to the next state that has not been explored. If a
-        game state leads to more than one unexplored game states,
-        explore in the order implied by the GameMaster.getMovables()
-        function.
-        If all game states reachable from a parent state has been explored,
-        the next explored state should conform to the specifications of
-        the Breadth-First Search algorithm.
+	def solveOneStep(self):
+		"""
+		Go to the next state that has not been explored. If a
+		game state leads to more than one unexplored game states,
+		explore in the order implied by the GameMaster.getMovables()
+		function.
+		If all game states reachable from a parent state has been explored,
+		the next explored state should conform to the specifications of
+		the Breadth-First Search algorithm.
 
-        Returns:
-            True if the desired solution state is reached, False otherwise
-        """
-        ### Student code goes here
-        return True
+		Returns:
+			True if the desired solution state is reached, False otherwise
+		"""
+		if self.currentState.state == self.victoryCondition:
+			return True
+
+		currDepth = self.currentState.depth
+		currMove = False
+		while self.currentState.parent:
+			self.gm.reverseMove(self.currentState.requiredMovable)
+			self.currentState = self.currentState.parent
+			counter = self.currentState.nextChildToVisit
+			if len(self.currentState.children) > counter:
+				currMove = True
+				break
+		if not currMove:
+			for visitedState in self.visited.keys():
+				visitedState.nextChildToVisit = 0
+			currDepth += 1
+			if len(self.visited) == 1:
+				possibleMoves = self.gm.getMovables()
+				for move in possibleMoves:
+					self.gm.makeMove(move)
+					newState = GameState(self.gm.getGameState(), currDepth, move)
+					newState.parent = self.currentState
+					self.visited[newState] = False
+					self.currentState.children.append(newState)
+					self.gm.reverseMove(move)
+		while currDepth != self.currentState.depth:
+			counter = self.currentState.nextChildToVisit
+			self.currentState.nextChildToVisit += 1
+			if len(self.currentState.children) > counter:
+				self.currentState = self.currentState.children[counter]
+				nextMove = self.currentState.requiredMovable
+				self.gm.makeMove(nextMove)
+			else:
+				currMove = False
+				while self.currentState.parent:
+					self.gm.reverseMove(self.currentState.requiredMovable)
+					self.currentState = self.currentState.parent
+					if len(self.currentState.children) > self.currentState.nextChildToVisit:
+						currMove = True
+						break
+				if not currMove:
+					return False
+
+		if self.currentState.state != self.victoryCondition:
+			self.visited[self.currentState] = True
+			possibleMoves = self.gm.getMovables()
+			newDepth = currDepth + 1
+			for move in possibleMoves:
+				self.gm.makeMove(move)
+				newState = GameState(self.gm.getGameState(), newDepth, move)
+				if newState not in self.visited:
+					self.visited[newState] = False
+					newState.parent = self.currentState
+					self.currentState.children.append(newState)
+				self.gm.reverseMove(move)
+			return False
+		else:
+		   return True
